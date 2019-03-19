8c167e381b808a6dac490dbf02d8b7bf6b41f2b1
Volha Kavaliova <vmatseralnik@scnsoft.com>
2019-03-19 15:34:00 +0300

    TestStickerPresence is added

---
+++ Csharp-example/csharp-example/MainPageStickerCheck.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+
+namespace csharp_example
+{
+    [TestFixture]
+    public class MainPageStickerCheck
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+        }
+
+        [Test]
+        public void TestStickerPresence()
+        {
+            driver.Url = "http://localhost:8080/litecart";
+            wait.Until(ExpectedConditions.ElementExists(By.Id("slider-wrapper")));
+
+            var productItems = driver.FindElements(By.CssSelector("li.product.column.shadow.hover-light"));
+            foreach(var productItem in productItems)
+            {
+                var stickerAmount=productItem.FindElements(By.CssSelector("[class^=sticker]")).Count;
+                Assert.AreEqual(1, stickerAmount);
+            }
+
+           
+            
+
+
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
